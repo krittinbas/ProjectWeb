99d84c2c702b93b7bfb376bcd858b3005b890a9f
krittinbas <kwongchanloy@gmail.com>
2023-10-14 17:09:18 +0700

    ...

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
 import './App.css';
-import Register from './Register/Register.js'
-
-function App() {
-  return (
-    <div>
-      <Register />
-=======
 import Login from './login/login';
 
-
 function App() {
-  return (
-    <div className="App">
-      <Login/>
->>>>>>> origin/Bass_Design
-    </div>
-  );
 }
 export default App;
