03c14871d9b8230310be2c13c83bd9dc6f3e2743
lnwskv <53941997+lnwskv@users.noreply.github.com>
2023-10-15 05:50:37 +0000

    import './App.css';

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import './App.css';
-import Register from './Register/Register.js'
 
 function App(){
-    return(
-        <div>
-            <Register />
-        </div>
-    );
 }
 
 export default App;
