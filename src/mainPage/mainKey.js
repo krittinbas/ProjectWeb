06801edb5bcfbcde7ebf533237a1f25e745b3975
Buglot <65818720+buglot@users.noreply.github.com>
2023-10-22 15:28:09 +0700

    peter n

--- src/mainPage/mainKey.js
+++ src/mainPage/mainKey.js
@@ -1,6 +1,9 @@
+import { useLocation } from "react-router-dom";
 import "./key.css"
 
 export default function MainKey(){
+    const location = useLocation()
+    console.log(location.state?.data)
     return (
         <div>
             key
