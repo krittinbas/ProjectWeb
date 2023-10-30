124425fb81cce837fcbb85568f5ed17154e6ab47
lnwskv <53941997+lnwskv@users.noreply.github.com>
2023-10-30 08:48:32 +0000

    Update routers

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { useLocation } from "react-router-dom";
 import { url_myAPI } from './config.js';
 import Login from './login/login.js';
-import WebBody from "./CopyFromPeter/webBody";
 import Register from './Register/Register';
-import Page_STATE from './history/history';
+import ForgotPasswordPage from './forgot/ForgotPasswordPage';
 import DoorControl from './firstpage/DoorControl';
+import Page_Manger_key from './keymanagement/page_manger_key';
+import OnlyHost from './HostManager/OnlyHost';
+import Page_STATE from './history/history';
+import SidebarDisplay from './Sidebar/SidebarDisplay';
 
 function App() {
+    const location = useLocation();
+    let userData = location.state?.data
+    const [infoAccount, setInfoAccount] = useState({});
+
+    useEffect(() => {
+        fetch(url_myAPI + "info?user=" + userData.user)
+            .then(response => response.json())
+            .then(data => {
+                setInfoAccount(data)
+            })
+    }, [setInfoAccount])
+
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/app" element={<WebBody />} />
-                <Route path="/DoorControl" element={<DoorControl/>}></Route>
-                <Route path="/his" element={<Page_STATE />} />
+                <Route path="/forgetpass" element={<ForgotPasswordPage />} />
+
+                <div>
+                    <SidebarDisplay info={infoAccount} />
+                    <Route path="/DoorControl" element={<DoorControl />}></Route>
+                    <Route path="/KeyManager" element={<Page_Manger_key info={infoAccount} />}></Route>
+                    <Route path="/KeyHost" element={<OnlyHost />}></Route>
+                    <Route path="/history" element={<Page_STATE info={infoAccount} />} />
+                </div>
+
                 <Route path="/login" element={<Login />} />
             </Routes>
         </Router>
--- src/Sidebar/SidebarDisplay.js
+++ src/Sidebar/SidebarDisplay.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './SidebarComponent/Sidebar';
+import DoorControl from '../firstpage/DoorControl';
 import Page_Manger_key from '../keymanagement/page_manger_key';
+import OnlyHost from '../HostManager/OnlyHost';
+import Page_STATE from '../history/history';
 
-export default function SidebarDisplay(){
-    return (
-        <Router>
-          <div>
-            <Sidebar />
-            <div className="content">
-              <Routes>
-                <Route path="/key" component={Page_Manger_key} />
-              </Routes>
-            </div>
-          </div>
-        </Router>
-      );
+export default function SidebarDisplay(prop) {
+  return (
+    <Router>
+      <div>
+        <Sidebar />
+        <div className="content">
+          <Routes>
+            <Route path="/DoorControl" element={<DoorControl />}></Route>
+            <Route path="/KeyManager" element={<Page_Manger_key info={prop.info} />}></Route>
+            <Route path="/KeyHost" element={<OnlyHost />}></Route>
+            <Route path="/history" element={<Page_STATE info={prop.info} />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
